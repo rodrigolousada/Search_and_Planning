21f0075859155b59b3dfd8a9fad2537bbc6ade90
rodrigolousada <rodrigo.m.lousada@gmail.com>
2018-12-06 23:15:55 +0000

    Successors maybe done

--- G05_Proj2.lisp
+++ G05_Proj2.lisp
@@ -56,9 +56,39 @@
 	(return-from make-state-copy new_state)
 )
 
-
-
-;; talvez seja necessario fazer sort
+;; Instrucoes de leitura para a Sofia:
+;;
+;;'''''''xxxxxx''''''xxxxxx
+;;'''''xxxxxxxxxx''xxxxxxxxxx
+;;''''xxxxxxxxxxxxxxxx''''xxxx
+;;'''xxxxxxxxxxxxxxxxxx''''xxxx
+;;'''xxxxxxxxxxxxxxxxxxx''xxxxx
+;;'''xxxxxxxxxxxxxxxxxxx 'xxxxx'
+;;'''''xxxxxxxxxxxxxxxxxxxxx
+;;''''''''xxxxxxxxxxxxxxxx
+;;''''''''''xxxxxxxxxxxxx
+;;''''''''''''xxxxxxxx
+;;''''''''''''''xxxx
+;;
+;;		1) Antes demais e fulcral entender que eu gosto muito dela
+;;		2) Esperar que tenha dormido bem
+;;		3) Admitir que eu estava a ser irritante
+;;		4) Compensa-la por isso e esperar que fique feliz com o que fiz xD
+;;
+;;		Agora relativo ao trabalho
+;;		1) Apenas passei para código o que estava na cabeça (que espero que esteja a funcionar)
+;;		2) Confere se concordas com o racicionio e/ou se me espalhei em algum lado
+;;		3) Para ser mais fácil acompanhar o processo acabei a estar sempre a nomear as coisas (para nao acabar com linhas muito grandes e imperceptiveis)
+;;		3.1) Eu sei que fica cheio de linhas de codigo e nao gostas
+;;		3.1.1) Mas ao menos usei us setf como gostas <3
+;;		4) Traducao para 4 siglas que vais ver nos nomes
+;;				ld -> local_departure
+;;				la -> local_arrival
+;;				id -> instant_departure
+;;				ia -> instant_arrival
+;;		5) Quando uso setfs para algum objecto comeco por fazer do departure (local e instant) e depois arrival (local e instant)
+;;		6) Gosto muito de trabalhar contigo		
+;;		7) Desculpa mais uma vez!
 
 (defun successors (state)
 	(setf list_successors (list))
@@ -68,54 +98,80 @@
 	(setf trip_non_al (nth 0 non_allocated) )
 	(setf state_trip_list (shifts-trips_list copy_state))
 	(setf (shifts-non_allocated_trips copy_state) (rest (shifts-non_allocated_trips copy_state)))
+	
+	;;Constants
+	(setf meal_treshold 240)
+	(setf shift_treshold 480)
+	(setf penalty 40)
+	(setf meal_time 40)
 
-	(dotimes (trip_i (length state_trip_list)  ) 
+	;;New to allocate
+	(setf new_ld (trip-local_departure trip_non_al))
+	(setf new_la (trip-local_arrival trip_non_al))
+	(setf new_id (trip-instant_departure trip_non_al))
+	(setf new_ia (trip-instant_arrival trip_non_al))
+	
+	
+	(if (equal 'L1 new_ld)
+				(setf new_start_penalty 0)
+				(setf new_start_penalty penalty))
+	(if (equal 'L1 new_la)
+				(setf new_end_penalty 0)
+				(setf new_end_penalty penalty))
+	
+	(setf new_id_if_first (+ new_id new_start_penalty))	
+	(setf new_ia_if_last (+ new_ia new_end_penalty))
+	
+	;;Possibilities of going in the end of an existing shift
+	(dotimes (shift_i (length state_trip_list)  )
+		;;Existing shift
+		(setf this_shift (nth shift_i state_trip_list))
+		(print (first this_shift))
+		(setf shift_ld (trip-local_departure (first this_shift)))
+		(if (equal 'L1 shift_ld)
+				(setf start_penalty 0)
+				(setf start_penalty penalty))
+		(setf shift_id (- (trip-instant_departure (first this_shift)) start_penalty))
 		
-			
-		(cond 
-			( 	(and
-					(or 	(and 	(equal (trip-local_departure trip_non_al) (trip-local_arrival (nth 0 (last (nth trip_i state_trip_list)))) )
-									(>= (trip-instant_departure trip_non_al) (trip-instant_arrival (nth 0 (last (nth trip_i state_trip_list) )))) ) 
-							(and 	(not (equal (trip-local_departure trip_non_al) (trip-local_arrival (nth 0 (last (nth trip_i state_trip_list)))) ))
-									(>= (trip-instant_departure trip_non_al) (+ (trip-instant_arrival (nth 0 (last (nth trip_i state_trip_list) ))) 40) ))
-					)
-					(or 	(or 	(and 	(and 	(equal L1 (trip-local_departure (nth 0 (first (nth trip_i state_trip_list)))) )
-													(equal L1 (trip-local_arrival trip_non_al) ))
-											(> 480 (- (trip-instant_arrival trip_non_al) (trip-instant_departure (nth 0 (first (nth trip_i state_trip_list)))))) )
-									(and 	(and 	(equal L1 (trip-local_departure (nth 0 (first (nth trip_i state_trip_list)))) )
-													(not (equal L1 (trip-local_arrival trip_non_al) )))
-											(> 480 (- (+ (trip-instant_arrival trip_non_al) 40) (trip-instant_departure (nth 0 (first (nth trip_i state_trip_list)))))))
-							)
-							(or 	(and 	(and 	(not (equal L1 (trip-local_departure (nth 0 (first (nth trip_i state_trip_list)))) ))
-													(equal L1 (trip-local_arrival trip_non_al) ))
-											(> 480 (- (trip-instant_arrival trip_non_al) (- (trip-instant_departure (nth 0 (first (nth trip_i state_trip_list)))) 40) )) )
-									(and 	(and 	(not (equal L1 (trip-local_departure (nth 0 (first (nth trip_i state_trip_list)))) ))
-													(not (equal L1 (trip-local_arrival trip_non_al) )))
-											(> 480 (- (+ (trip-instant_arrival trip_non_al) 40) (- (trip-instant_departure (nth 0 (first (nth trip_i state_trip_list)))) 40) )))
-							)
-					)
-				)
-				( (setf last_meal (trip-local_departure (nth 0 (first (nth trip_i state_trip_list)))))
-					(if (or	(<= (- (trip-instant_arrival trip_non_al) last_meal) 240)
-							(<=    (+ (trip-local_arrival (nth 0 (last (nth trip_i state_trip_list)))) 40) (trip-instant_departure trip_non_al))
-						)
-						(setf new_copy_state (make-state-copy copy_state))
-						(setf (nth trip_i (shifts-trips_list new_copy_state)) (append (nth trip_i (shifts-trips_list new_copy_state)) (list trip_non_al)) )
-						(setf list_successors (append list_successors (list new_copy_state)))	
-					)
-				)
-			)
-
-			
+		(setf shift_la (trip-local_arrival (nth 0 (last this_shift))))
+		(setf shift_ia (trip-instant_arrival (nth 0 (last this_shift))))
+		(if (equal shift_la new_ld)
+				(setf change_station_penalty 0)
+				(setf change_station_penalty penalty))
+		
+		;;Get Last meal time
+		(setf last_meal shift_id)
+		(dotimes (trip_i (- (length state_trip_list) 1))
+			(setf this_trip (nth trip_i this_shift))
+			(setf next_trip (nth (+ trip_i 1) this_shift))
+			(setf this_ia (trip-instant_arrival this_trip))
+			(setf next_id (trip-instant_departure next_trip))
+			(setf break_between (- next_id this_ia))
+			(cond ((>= break_between meal_time) (setf last_meal next_id)))
 		)
 		
+		(cond 	(
+				(and 	
+					(and	(>= new_id (+ shift_ia change_station_penalty))
+							(>= shift_treshold (- new_ia_if_last shift_id))
+					)
+					(or 	(>= meal_treshold (- new_ia last_meal))
+							(>= new_id (+ shift_ia meal_treshold))
+					)
+				)
+					(setf new_copy_state (make-state-copy copy_state))
+					(setf (nth shift_i (shifts-trips_list new_copy_state)) (append (nth shift_i (shifts-trips_list new_copy_state)) (list trip_non_al)) )
+					(setf list_successors (append list_successors (list new_copy_state)))
+				)
+		)	
 	)
 	
+	;;Possibility of this trip create a new shift
 	(setf new_copy_state (make-state-copy copy_state))
 	(setf (shifts-trips_list new_copy_state) (append (shifts-trips_list new_copy_state) (list (list trip_non_al) )))
 	(setf list_successors (append list_successors (list new_copy_state)))
 	
-	; (return-from successors list_successors)
+	(return-from successors list_successors)
 )
 
 (defun is-goal (state)
@@ -123,8 +179,9 @@
 )
 
 (defun create-trip-prob (prob)
+	;; talvez seja necessario fazer sort do prob
 	(setf list_trips '())
-	(print prob)
+	;;(print prob)
 	(dolist (e  prob)
 		(setf list_trips (append list_trips (list (make-trip :local_departure (nth 0 e)
 															 :local_arrival (nth 1 e)
