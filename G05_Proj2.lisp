71e02d61a6d9a14ed0abd3b6d936081eb52e2ba0
rodrigolousada <rodrigo.m.lousada@gmail.com>
2018-12-07 16:50:07 +0000

    Some bugs fixed and first implementation of A star
    Done:
    - First implementation of A Star
    - Bug corrected on gen successors
    - Peanlty give to cost
    
    To do:
    - Fix solution of a star

--- G05_Proj2.lisp
+++ G05_Proj2.lisp
@@ -69,12 +69,53 @@
 	(return-from create-trip-prob (sort-list list_trips))
 )
 
-;; Makes deep copy of our structur shifts
+;; Makes deep copy of our structure shifts
 (defun make-state-copy (state)
 	(setf new_state (make-shifts 	
 							:trips_list (copy-list (shifts-trips_list state))
 							:non_allocated_trips (copy-list (shifts-non_allocated_trips state) )))
 	(return-from make-state-copy new_state)
+)
+
+;; Check if two trips are the same
+(defun trip-is-equal (trip1 trip2)
+	(return-from trip-is-equal (equalp trip1 trip2))
+)
+						
+;; Check if two states are equal
+(defun state-is-equal (state1 state2)
+	(setf s1_trip_list (shifts-trips_list state1))
+	(setf s2_trip_list (shifts-trips_list state2))
+	(setf s1_non_alloc_trips (shifts-non_allocated_trips state1))
+	(setf s2_non_alloc_trips (shifts-non_allocated_trips state2))
+	
+	;;if arrays are not the same length
+	(cond ((or 	(not (equal (length s1_trip_list) (length s2_trip_list)))
+				(not (equal (length s1_non_alloc_trips) (length s2_non_alloc_trips))))
+			(return-from state-is-equal nil))
+	)
+	
+	;;If list non alloc is not equal
+	(dotimes (i (length s1_non_alloc_trips))
+		(cond ((not (trip-is-equal (nth i s1_non_alloc_trips) (nth i s2_non_alloc_trips)))
+			(return-from state-is-equal nil))
+		)
+	)
+	
+	;; Go through every shift
+	(dotimes (shift_index (length s1_trip_list))
+		;; Check if shift sizes is the same
+		(cond ((not (equal (length (nth shift_index s1_trip_list)) (length (nth shift_index s2_trip_list))))
+			(return-from state-is-equal nil))
+		)
+		;; Check if shift trips are the same
+		(dotimes (i (length (nth shift_index s1_trip_list)))
+			(cond ((not (trip-is-equal (nth i (nth shift_index s1_trip_list)) (nth i (nth shift_index s2_trip_list))))
+				(return-from state-is-equal nil))
+			)
+		)
+	)
+	(return-from state-is-equal T)
 )
 
 
@@ -106,16 +147,137 @@
 ;; goal is to minimize the cost
 
 (defun cost-size-shifts (state)
+	;;(format t "entrou no cost~%")
 	(setf cost 0)
-	(dolist (listshift (shifts-trips_list state))
-		(setf cost (+ cost (- (trip-instant_arrival (nth 0 (last listshift))) (trip-instant_departure (first listshift)))))
+	(dolist (shift (shifts-trips_list state))
+		(cond ((not (equal 'L1 (trip-local_departure (first shift))))
+				(setf cost (+ cost 40))))
+		(cond ((not(equal 'L1 (trip-local_arrival (nth 0 (last shift)))))
+				(setf cost (+ cost 40))))
+		(setf cost (+ cost (- (trip-instant_arrival (nth 0 (last shift))) (trip-instant_departure (first shift)))))
 	)
 	(return-from cost-size-shifts cost )
+	;;(format t "saiu do cost~%")
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;                     Auxiliar Functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;	ld -> local_departure
+;;	la -> local_arrival
+;;	id -> instant_departure
+;;	ia -> instant_arrival
+
+(defun successors (state)
+	;;(format t "Got inside sucessores~%")
+	(setf list_successors (list))
+	(setf copy_state (make-state-copy state))
+
+	(setf non_allocated (shifts-non_allocated_trips copy_state))
+	(setf trip_non_al (nth 0 non_allocated) )
+	(setf state_trip_list (shifts-trips_list copy_state))
+	(setf (shifts-non_allocated_trips copy_state) (rest (shifts-non_allocated_trips copy_state)))
+	
+	;;Constants
+	(setf meal_treshold 240)
+	(setf shift_treshold 480)
+	(setf penalty 40)
+	(setf meal_time 40)
+
+	;;New to allocate
+	(setf new_ld (trip-local_departure trip_non_al))
+	(setf new_la (trip-local_arrival trip_non_al))
+	(setf new_id (trip-instant_departure trip_non_al))
+	(setf new_ia (trip-instant_arrival trip_non_al))
+	
+	
+	(if (equal 'L1 new_ld)
+				(setf new_start_penalty 0)
+				(setf new_start_penalty penalty))
+	(if (equal 'L1 new_la)
+				(setf new_end_penalty 0)
+				(setf new_end_penalty penalty))
+	
+	(setf new_id_if_first (+ new_id new_start_penalty))	
+	(setf new_ia_if_last (+ new_ia new_end_penalty))
+	
+	;;(format t "New is ready~%")
+	
+	;;Possibilities of going in the end of an existing shift
+	(dotimes (shift_i (length state_trip_list)  )
+		;;Existing shift
+		(setf this_shift (nth shift_i state_trip_list))
+		;;(print (first this_shift))
+		(setf shift_ld (trip-local_departure (first this_shift)))
+		(if (equal 'L1 shift_ld)
+				(setf start_penalty 0)
+				(setf start_penalty penalty))
+		(setf shift_id (- (trip-instant_departure (first this_shift)) start_penalty))
+		
+		(setf shift_la (trip-local_arrival (nth 0 (last this_shift))))
+		(setf shift_ia (trip-instant_arrival (nth 0 (last this_shift))))
+		(if (equal shift_la new_ld)
+				(setf change_station_penalty 0)
+				(setf change_station_penalty penalty))
+		
+		;;Get Last meal time
+		(setf last_meal shift_id)
+		(dotimes (trip_i (- (length this_shift) 1))
+			(setf this_trip (nth trip_i this_shift))
+			(setf next_trip (nth (+ trip_i 1) this_shift))
+			(setf this_ia (trip-instant_arrival this_trip))
+			(setf next_id (trip-instant_departure next_trip))
+			(setf break_between (- next_id this_ia))
+			(cond ((>= break_between meal_time) (setf last_meal next_id)))
+		)
+		
+		(cond 	(
+				(and 	
+					(and	(>= new_id (+ shift_ia change_station_penalty))
+							(>= shift_treshold (- new_ia_if_last shift_id))
+					)
+					(or 	(>= meal_treshold (- new_ia last_meal))
+							(>= new_id (+ shift_ia meal_treshold))
+					)
+				)
+					(setf new_copy_state (make-state-copy copy_state))
+					(setf (nth shift_i (shifts-trips_list new_copy_state)) (append (nth shift_i (shifts-trips_list new_copy_state)) (list trip_non_al)) )
+					(setf list_successors (append list_successors (list new_copy_state)))
+				)
+		)	
+	)
+	
+	;;(format t "no more available shifts, lets create on for us~%")
+	
+	;;Possibility of this trip create a new shift
+	(setf new_copy_state (make-state-copy copy_state))
+	(setf (shifts-trips_list new_copy_state) (append (shifts-trips_list new_copy_state) (list (list trip_non_al) )))
+	(setf list_successors (append list_successors (list new_copy_state)))
+	
+	;;(format t "Getting out of successores with: ~%")
+	;;(dolist (successor list_successors)
+	;;	(print (translate_solution successor))
+	;;)
+	;;(format t "It's ouuuuuttt~%")
+	(return-from successors list_successors)
+)
+
+(defun is-goal (state)
+	(= 0 (length (shifts-non_allocated_trips state)))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;                     Heuristic functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun h-a-star-best-heuristic (state)
+ 	(if (is-goal state) (return-from a-star-heuristic 0))
 )
 
 (defun cost-shift-quant (state)
-	(setf quant_shifts (list-length (shifts-trips_list state)))
-	(return-from cost-shift-quant quant_shifts*10)
+	(setf quant_shifts (length (shifts-trips_list state)))
+	(return-from cost-shift-quant (* quant_shifts 10))
 )
 
 (defun cost-before-six (state)
@@ -132,7 +294,7 @@
 	(setf inc_waste 0)
 	(dolist (listshift (shifts-trips_list state))
 		
-		(dotimes (shift_index (list-length listshift))
+		(dotimes (shift_index (length listshift))
 					
 					(setf flag 0)
 					(cond 	( 	(equal shift_index 0)
@@ -196,115 +358,34 @@
 )
 
 
-
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;                     Auxiliar Functions
+;;;                     Algorithm functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;	ld -> local_departure
-;;	la -> local_arrival
-;;	id -> instant_departure
-;;	ia -> instant_arrival
+;;(defun melhor-abordagem (state)
+;;)
 
-(defun successors (state)
-	(setf list_successors (list))
-	(setf copy_state (make-state-copy state))
-
-	(setf non_allocated (shifts-non_allocated_trips copy_state))
-	(setf trip_non_al (nth 0 non_allocated) )
-	(setf state_trip_list (shifts-trips_list copy_state))
-	(setf (shifts-non_allocated_trips copy_state) (rest (shifts-non_allocated_trips copy_state)))
-	
-	;;Constants
-	(setf meal_treshold 240)
-	(setf shift_treshold 480)
-	(setf penalty 40)
-	(setf meal_time 40)
-
-	;;New to allocate
-	(setf new_ld (trip-local_departure trip_non_al))
-	(setf new_la (trip-local_arrival trip_non_al))
-	(setf new_id (trip-instant_departure trip_non_al))
-	(setf new_ia (trip-instant_arrival trip_non_al))
-	
-	
-	(if (equal 'L1 new_ld)
-				(setf new_start_penalty 0)
-				(setf new_start_penalty penalty))
-	(if (equal 'L1 new_la)
-				(setf new_end_penalty 0)
-				(setf new_end_penalty penalty))
-	
-	(setf new_id_if_first (+ new_id new_start_penalty))	
-	(setf new_ia_if_last (+ new_ia new_end_penalty))
-	
-	;;Possibilities of going in the end of an existing shift
-	(dotimes (shift_i (length state_trip_list)  )
-		;;Existing shift
-		(setf this_shift (nth shift_i state_trip_list))
-		; (print (first this_shift))
-		(setf shift_ld (trip-local_departure (first this_shift)))
-		(if (equal 'L1 shift_ld)
-				(setf start_penalty 0)
-				(setf start_penalty penalty))
-		(setf shift_id (- (trip-instant_departure (first this_shift)) start_penalty))
-		
-		(setf shift_la (trip-local_arrival (nth 0 (last this_shift))))
-		(setf shift_ia (trip-instant_arrival (nth 0 (last this_shift))))
-		(if (equal shift_la new_ld)
-				(setf change_station_penalty 0)
-				(setf change_station_penalty penalty))
-		
-		;;Get Last meal time
-		(setf last_meal shift_id)
-		(dotimes (trip_i (- (length state_trip_list) 1))
-			(setf this_trip (nth trip_i this_shift))
-			(setf next_trip (nth (+ trip_i 1) this_shift))
-			(setf this_ia (trip-instant_arrival this_trip))
-			(setf next_id (trip-instant_departure next_trip))
-			(setf break_between (- next_id this_ia))
-			(cond ((>= break_between meal_time) (setf last_meal next_id)))
-		)
-		
-		(cond 	(
-				(and 	
-					(and	(>= new_id (+ shift_ia change_station_penalty))
-							(>= shift_treshold (- new_ia_if_last shift_id))
-					)
-					(or 	(>= meal_treshold (- new_ia last_meal))
-							(>= new_id (+ shift_ia meal_treshold))
-					)
-				)
-					(setf new_copy_state (make-state-copy copy_state))
-					(setf (nth shift_i (shifts-trips_list new_copy_state)) (append (nth shift_i (shifts-trips_list new_copy_state)) (list trip_non_al)) )
-					(setf list_successors (append list_successors (list new_copy_state)))
-				)
-		)	
-	)
-	
-	;;Possibility of this trip create a new shift
-	(setf new_copy_state (make-state-copy copy_state))
-	(setf (shifts-trips_list new_copy_state) (append (shifts-trips_list new_copy_state) (list (list trip_non_al) )))
-	(setf list_successors (append list_successors (list new_copy_state)))
-	
-	(return-from successors list_successors)
+(defun a-star-best-heuristic (state)
+	(setf int-prob  (cria-problema state 
+									(list #'successors) 
+									:objectivo? #'is-goal 
+									:estado= #'state-is-equal 
+									:heuristica #'cost-shift-quant))
+	(setf best-state (last (first (procura int-prob "a*" :espaco-em-arvore? t))))
+	(return-from a-star-best-heuristic (nth 0 best-state))
 )
 
-(defun is-goal (state)
-	(= 0 (list-length non_allocated_trips)) ;;(shifts-non_allocated_trips state)
-)
+;;(defun a-star-alterantive-heuristic (state)
+;;)
 
-(
-)
+;;(defun sondagem-iterativa (state)
+;;)
 
+;;(defun ilds (state)
+;;)
 
-; (defun a-star-heuristic (state)
-; 	(if (is-goal state) (return-from a-star-heuristic 0))
-
-; )
-
+;;(defun abordagem-alternativa (state)
+;;)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;                     Solution function
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -316,62 +397,48 @@
 		  (max_trip_time 240) ;;max tempo sem pausa
 		  (min_duration_shift 360)
 		  (result_state nil)
-		  (all_shifts (make-shifts :trips_list '() :non_allocated_trips (create-trip-prob problem) ))
+		  (root_state (make-shifts :trips_list '() :non_allocated_trips (create-trip-prob problem) ))
 		)
 		
 		(cond ((equal strategy "melhor.abordagem") 
-				(setf result_state (melhor-abordagem problem)))
+				(setf result_state (melhor-abordagem root_state)))
 			  ((equal strategy "a*.melhor.heuristica") 
-				(setf result_state (a-star-best-heuristic all_shifts)))
+				(setf result_state (a-star-best-heuristic root_state)))
 			  ((equal strategy "a*.melhor.heuristica.alternativa") 
-				(setf result_state (a-star-alternative-heuristic problem)))
+				(setf result_state (a-star-alternative-heuristic root_state)))
 			  ((equal strategy "sondagem.iterativa") 
-				(setf result_state (sondagem-iterativa problem)))
+				(setf result_state (sondagem-iterativa root_state)))
 			  ((equal strategy "ILDS") 
-				(setf result_state (ilds problem)))
+				(setf result_state (ilds root_state)))
 			  ((equal strategy "abordagem.alternativa")
-				(setf result_state (melhor-abordagem problem))
+				(setf result_state (abordagem-alternativa root_state))
 			)
 		)
+		(return-from faz-afectacao (translate_solution result_state))
 	)
 )
 
-
-;;; melhor-abordagem - Iterative a* and estimate-time-left-part-sequential-part-paralel heuristic
-; (defun a-star-best-heuristic problem (problem)
-; 	(setf initial_state problem)
-
-; 	(setf initial_prob (cria-problema initial_state
-; 							(list #'sucessors)
-; 						)
-; 	)
-
-
-
-; 	; (iterative-search (cria-problema (job-shop-problem-to-job-state problem) 
-; 	; 				 		(list #'sucessors)
-; 	; 					   	:objectivo? #'objective? 
-; 	; 					   	:heuristica #'estimate-time-left-part-sequential-part-paralel
-; 	; 					   	:custo #'cost-transition-max-machines
-; 	; 					)
-; 	; )
-; )
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;                        Tests
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; (setf gentest (faz-afectacao '( (L2 L1 1 25) (L1 L2 34 60) (L5 L1 408 447) (L1 L1 448 551) (L1 L1 474 465) ) "melhor.abordagem"))
-; (setf test_state (make-shifts :trips_list '() :non_allocated_trips (create-trip-prob '( (L2 L1 1 25) (L1 L2 34 60) (L5 L1 408 447)))))
+(setf gentest (faz-afectacao '( (L2 L1 1 25) (L1 L2 34 60) (L5 L1 408 447) (L1 L1 448 551) (L1 L1 474 465) ) "a*.melhor.heuristica"))
+(setf test_state (make-shifts :trips_list '() :non_allocated_trips (create-trip-prob '( (L2 L1 1 25) (L1 L2 34 60) (L5 L1 408 447)))))
 
-; (setf test_state_suc (make-shifts :trips_list (list (create-trip-prob '( (L2 L1 1 25)))) :non_allocated_trips (create-trip-prob '(  (L3 L2 65 80) (L5 L1 408 447)))))
+(setf test_state_suc_1 (make-shifts :trips_list (list (create-trip-prob '( (L2 L1 1 25)))) :non_allocated_trips (create-trip-prob '(  (L3 L2 65 80) (L5 L1 408 447)))))
 
+(setf test_state_suc_2 (make-shifts :trips_list (list (create-trip-prob '( (L2 L1 1 25)))) :non_allocated_trips (create-trip-prob '(  (L3 L2 65 80) (L5 L1 408 447)))))
+
+; (setf test_state_suc_3 (make-shifts :trips_list (list (create-trip-prob '( (L2 L1 1 25)))) :non_allocated_trips (create-trip-prob '(  (L3 L2 65 80) (L5 L2 408 447)))))
 ; (setf test_suc (make-shifts :trips_list (list (create-trip-prob '( (L2 L3 1 100) (L4 L1 190 200) )  ) ) :non_allocated_trips (create-trip-prob '( (L5 L6 300 480)))) )
 
 ; (setf test_sort  (create-trip-prob '((L1 L4 100 300) (L4 L3 10 40) (L5 L8 30 100) (L6 L6 20 40)))) 
 
 ; (setf test_cost_time_waste (cost-time-wasted (make-shifts 	:trips_list (list (create-trip-prob '( (L2 L3 1 100) (L4 L1 190 200) )  ) ) 
 ; 															:non_allocated_trips '() )))
-; (setf test_cost_size_shifts (cost-size-shifts (make-shifts 	:trips_list (list (create-trip-prob '( (L2 L3 1 100) (L4 L1 190 200) )  ) ) 
-; 															:non_allocated_trips '() )))
-(setf test_cost_before_six (cost-before-six (make-shifts 	:trips_list (list (create-trip-prob '( (L2 L3 1 100) (L4 L1 190 200) )  ) ) 
-															:non_allocated_trips '() )))
+(setf test_cost_size_shifts (cost-size-shifts (make-shifts 	:trips_list (list (create-trip-prob '( (L2 L3 1 100) (L4 L2 190 200) )  ) ) 
+ 															:non_allocated_trips '() )))
+; (setf test_cost_before_six (cost-before-six (make-shifts 	:trips_list (list (create-trip-prob '( (L2 L3 1 100) (L4 L1 190 200) )  ) ) 
+;															:non_allocated_trips '() )))
+
+(setf test_a_star (translate_solution (a-star-best-heuristic test_state)))
